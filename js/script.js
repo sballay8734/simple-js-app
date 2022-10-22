eee0fc3876fc5cc6c1cf90d9e29f9f8d34147624
sballay8734 <48398020+sballay8734@users.noreply.github.com>
2022-10-22 10:19:16 -0400

    Updated the format of pokemon object list

--- js/script.js
+++ js/script.js
@@ -1,9 +1,37 @@
 pokemonList = [
-    {name: 'Bulbasaur', height: 0.7, types: ['Grass', 'Poison']},
-    {name: 'Charmander', height: 0.6, types: ['Fire']},
-    {name: 'Squirtle', height: 0.5, types: ['Water']},
-    {name: 'Arcanine', height: 1.9, types: ['Fire']},
-    {name: 'Gengar', height: 1.5, types: ['Ghost', 'Poison']},
-    {name: 'Gyarados', height: 6.5, types: ['Water', 'Flying']},
-    {name: 'Zapdos', height: 1.6, types: ['Electric', 'Flying']},
+    {
+        name: 'Bulbasaur', 
+        height: 0.7, 
+        types: ['Grass', 'Poison']
+    },
+    {
+        name: 'Charmander', 
+        height: 0.6, 
+        types: ['Fire']
+    },
+    {
+        name: 'Squirtle', 
+        height: 0.5, 
+        types: ['Water']
+    },
+    {
+        name: 'Arcanine', 
+        height: 1.9, 
+        types: ['Fire']
+    },
+    {
+        name: 'Gengar', 
+        height: 1.5, 
+        types: ['Ghost', 'Poison']
+    },
+    {
+        name: 'Gyarados', 
+        height: 6.5, 
+        types: ['Water', 'Flying']
+    },
+    {
+        name: 'Zapdos', 
+        height: 1.6, 
+        types: ['Electric', 'Flying']
+    },
 ];
