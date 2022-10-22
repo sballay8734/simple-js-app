284d0dee62edcf3d9a13ef46288e859bf7be9cbd
sballay8734 <48398020+sballay8734@users.noreply.github.com>
2022-10-22 19:05:59 -0400

    Testing continued

--- js/test-script.js
+++ js/test-script.js
@@ -1,30 +1,53 @@
-let myName = 'Shawn';
-let greeting = 'Hello';
+// let myName = 'Shawn';
+// let greeting = 'Hello';
 
-console.log(`${greeting} ${myName}!`);
+// console.log(`${greeting} ${myName}!`);
 
-/////////////////////////////////////////////// Break
+////////////////////////////////////////////////////////////////////////// Break
 
-let currentModel = 'Chevy';
+// let currentModel = 'Chevy';
 
-let car = {
-    color: 'red',
-    mileage: 10,
-    make: 'BMW',
-    size: 'small',
-    brother: {
-        name: 'kyle',
-        age: 34,
-    }
-};
+// let car = {
+//     color: 'red',
+//     mileage: 10,
+//     make: 'BMW',
+//     size: 'small',
+//     brother: {
+//         name: 'kyle',
+//         age: 34,
+//     }
+// };
 
-car[currentModel] = 13;
-console.log(car[currentModel]);
+// car[currentModel] = 13;
+// console.log(car[currentModel]);
 
-let friendList = [
-    {friendName: 'Aaron', friendAge: 31, job: 'Teacher'},
-    {friendName: 'Donnie', friendAge: 31, job: 'IT'},
-    {},
-    {},
-    {},
-]
+// let friendList = [
+//     {friendName: 'Aaron', friendAge: 31, job: 'Teacher'},
+//     {friendName: 'Donnie', friendAge: 31, job: 'IT'},
+//     {},
+//     {},
+//     {},
+// ]
+
+
+
+//////////////////////////////////////////////////////////// Conditional Testing
+
+// let age = 45;
+
+// if (age < 45) {
+//     console.log("You are young!");
+// } else {
+//     console.log("You're getting old!...");
+// }
+
+// if (age === 45 || age == 23) {
+//     console.log('45 or 23');
+// } else {
+//     console.log('not 45 or 23');
+// }
+
+value = 'James';
+userName = value || 'friend';
+
+console.log(`Hi there ${userName}!`)
