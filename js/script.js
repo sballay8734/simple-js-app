65e1abbb04fe73c226338ed63548712d8d88f3f8
sballay8734 <48398020+sballay8734@users.noreply.github.com>
2022-10-29 14:59:13 -0400

    Updated for Tutor

--- js/script.js
+++ js/script.js
@@ -156,44 +156,44 @@ printArrayDetails2(pokemonRepository.getAll());
 // }
 
 // printArrayDetails(pokemonList);
-let testList = [
-    {
-        name: 'Bulbasaur', 
-        height: 0.7, 
-        types: ['Grass', 'Poison']
-    },
-    {
-        name: 'Charmander', 
-        height: 0.6, 
-        types: ['Fire']
-    },
-    {
-        name: 'Squirtle', 
-        height: 0.5, 
-        types: ['Water']
-    },
-    {
-        name: 'Arcanine', 
-        height: 1.9, 
-        types: ['Fire']
-    },
-    {
-        name: 'Gengar', 
-        height: 1.5, 
-        types: ['Ghost', 'Poison']
-    },
-    {
-        name: 'Gyarados', 
-        height: 6.5, 
-        types: ['Water', 'Flying']
-    },
-    {
-        name: 'Zapdos', 
-        height: 1.6, 
-        types: ['Electric', 'Flying']
-    },
-];
+// let testList = [
+//     {
+//         name: 'Bulbasaur', 
+//         height: 0.7, 
+//         types: ['Grass', 'Poison']
+//     },
+//     {
+//         name: 'Charmander', 
+//         height: 0.6, 
+//         types: ['Fire']
+//     },
+//     {
+//         name: 'Squirtle', 
+//         height: 0.5, 
+//         types: ['Water']
+//     },
+//     {
+//         name: 'Arcanine', 
+//         height: 1.9, 
+//         types: ['Fire']
+//     },
+//     {
+//         name: 'Gengar', 
+//         height: 1.5, 
+//         types: ['Ghost', 'Poison']
+//     },
+//     {
+//         name: 'Gyarados', 
+//         height: 6.5, 
+//         types: ['Water', 'Flying']
+//     },
+//     {
+//         name: 'Zapdos', 
+//         height: 1.6, 
+//         types: ['Electric', 'Flying']
+//     },
+// ];
 
-testList.forEach(function(item){
-    console.log(Object.keys(item))
-});
+// testList.forEach(function(item){
+//     console.log(Object.keys(item))
+// });
