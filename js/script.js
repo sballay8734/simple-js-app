3f5e4adb63caf049c750b0b29a3278acaa638bb0
sballay8734 <48398020+sballay8734@users.noreply.github.com>
2022-11-09 07:49:48 -0500

    Task 7

--- js/script.js
+++ js/script.js
@@ -1,51 +1,28 @@
 let pokemonRepository = (function () {
-    let pokemonList = [
-        {
-            name: 'Bulbasaur', 
-            height: 0.7, 
-            types: ['Grass', 'Poison'],
-            // testing functions
-            details: 'Grass and poison type pokemon that precedes Ivysaur and Venusaur'
-        },
-        {
-            name: 'Charmander', 
-            height: 0.6, 
-            types: ['Fire']
-        },
-        {
-            name: 'Squirtle', 
-            height: 0.5, 
-            types: ['Water']
-        },
-        {
-            name: 'Arcanine', 
-            height: 1.9, 
-            types: ['Fire']
-        },
-        {
-            name: 'Gengar', 
-            height: 1.5, 
-            types: ['Ghost', 'Poison']
-        },
-        {
-            name: 'Gyarados', 
-            height: 6.5, 
-            types: ['Water', 'Flying']
-        },
-        {
-            name: 'Zapdos', 
-            height: 1.6, 
-            types: ['Electric', 'Flying']
-        },
-    ];
+
+    let pokemonList = [];
+    let apiURL = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
     // returns pokemon list of objects
+    // function titleCase() {
+    //     return str.replace(
+    //         /\w\S*/g,
+    //         function(txt) {
+    //           return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    //         }
+    //       );
+    // }
+
     function getAll() {
         return pokemonList;
     }
 
     function add(object) {
-        if ((typeof object === 'object') && JSON.stringify(Object.keys(object).sort()) === JSON.stringify(['height', 'name', 'types'])) {
+        if ((typeof object === 'object')) {
+            object = {
+                name: object.name,
+                detailsUrl: object.detailsUrl
+            }
             pokemonList.push(object);
         } else {
             return "Incorrect Format";
@@ -56,8 +33,7 @@ let pokemonRepository = (function () {
         let listSelector = document.querySelector('ul');
         let listItem = document.createElement('li');
         let buttonElement = document.createElement('button');
-        buttonElement.innerText = pokemon.name;
-        buttonElement.classList.add(pokemon.types[0].toLowerCase());
+        buttonElement.innerText = pokemon.name.toUpperCase();
         listItem.appendChild(buttonElement);
         listSelector.appendChild(listItem);
         // call function to add listener
@@ -68,160 +44,59 @@ let pokemonRepository = (function () {
         buttonElement.addEventListener('click', () => showDetails(pokemon));
     }
 
-    // not working as intended
     function showDetails(pokemon) {
-        // let parent = document.querySelector('li')
-        // let detailSpace = document.createElement('p');
-        // detailSpace.innerText = pokemon.types;
-        // parent.appendChild(detailSpace);
-        // detailSpace.classList.add('hidden');
-        // detailSpace.classList.toggle('is-visible');
-        console.log(pokemon);
-    };
+        loadDetails(pokemon).then(() => {
+            console.log(pokemon);
+        });
+    }
+
+    function loadList() {
+        return fetch(apiURL).then((response) => {
+            return response.json();
+        }).then((json) => {
+            json.results.forEach((item) => {
+                let pokemon = {
+                    name: item.name,
+                    detailsUrl: item.url 
+                };
+                add(pokemon);
+            });
+        }).catch((error) => {
+            console.error(error);
+        })
+    }
+
+    function loadDetails(item) {
+        let url = item.detailsUrl;
+        return fetch(url).then((response) => {
+            return response.json();
+        }).then((details) => {
+            item.imageUrl = details.sprites.front_default;
+            item.height = details.height;
+            item.types = details.types;
+        }).catch((error) => {
+            console.error(error)
+        })
+    }
 
     return {
         getAll: getAll,
         addListItem: addListItem,
         add: add,
         showDetails: showDetails,
-        addListener: addListener
+        addListener: addListener,
+        loadList: loadList,
+        loadDetails: loadDetails,
     };
 
 })();
 
-function printArrayDetails(list) {
-    list.forEach(pokemon => {
+pokemonRepository.loadList().then( () => {
+    pokemonRepository.getAll().forEach((pokemon) => {
         pokemonRepository.addListItem(pokemon);
     });
-}
+});
 
-pokemonRepository.add({name: 'Ninetales', height: 1.1, types: ['Fire']});
-printArrayDetails(pokemonRepository.getAll());
+
 
 // ************************** OLD CODE BELOW ************************** 
-
-// pokemonList = [
-//     {
-//         name: 'Bulbasaur', 
-//         height: 0.7, 
-//         types: ['Grass', 'Poison']
-//     },
-//     {
-//         name: 'Charmander', 
-//         height: 0.6, 
-//         types: ['Fire']
-//     },
-//     {
-//         name: 'Squirtle', 
-//         height: 0.5, 
-//         types: ['Water']
-//     },
-//     {
-//         name: 'Arcanine', 
-//         height: 1.9, 
-//         types: ['Fire']
-//     },
-//     {
-//         name: 'Gengar', 
-//         height: 1.5, 
-//         types: ['Ghost', 'Poison']
-//     },
-//     {
-//         name: 'Gyarados', 
-//         height: 6.5, 
-//         types: ['Water', 'Flying']
-//     },
-//     {
-//         name: 'Zapdos', 
-//         height: 1.6, 
-//         types: ['Electric', 'Flying']
-//     },
-// ];
-
-// printArrayDetails(pokemonList);
-// let testList = [
-//     {
-//         name: 'Bulbasaur', 
-//         height: 0.7, 
-//         types: ['Grass', 'Poison']
-//     },
-//     {
-//         name: 'Charmander', 
-//         height: 0.6, 
-//         types: ['Fire']
-//     },
-//     {
-//         name: 'Squirtle', 
-//         height: 0.5, 
-//         types: ['Water']
-//     },
-//     {
-//         name: 'Arcanine', 
-//         height: 1.9, 
-//         types: ['Fire']
-//     },
-//     {
-//         name: 'Gengar', 
-//         height: 1.5, 
-//         types: ['Ghost', 'Poison']
-//     },
-//     {
-//         name: 'Gyarados', 
-//         height: 6.5, 
-//         types: ['Water', 'Flying']
-//     },
-//     {
-//         name: 'Zapdos', 
-//         height: 1.6, 
-//         types: ['Electric', 'Flying']
-//     },
-// ];
-// console.log(testList[0].types[0]);
-// let headerArea = document.querySelector('header');
-// let button = document.createElement('button');
-// button.innerText = testList[0].name;
-// headerArea.appendChild(button);
-// button.classList.add(testList[0].types[1].toLowerCase());
-
-// testList.forEach(pokemon => {
-// console.log(pokemon.types);
-// })
-
-// testList.forEach(function(item){
-//     console.log(Object.keys(item))
-// });
-
-
-// let header = document.querySelector('header')
-// let button = document.createElement('button');
-// button.innerText = 'Click Me';
-// header.appendChild(button);
-
-///////// CHALLENGES BELOW //////////
-
-// add internal button & sibling button to header div
-// let header = document.querySelector('header');
-// let button2 = document.createElement('button');
-// button2.innerText = 'Sibling Button';
-// header.appendChild(button2);
-
-// let headerDiv = document.querySelector('header > div');
-// let divButton = document.createElement('button');
-// divButton.innerText = 'Header Inside Div Button';
-// headerDiv.appendChild(divButton);
-
-// // add paragraph to center main div
-// let middleDiv = document.querySelector('main :nth-child(2)');
-// let middleDivPara = document.createElement('p');
-// middleDivPara.innerText = 'Paragraph inside middle div';
-// middleDiv.appendChild(middleDivPara);
-
-
-// // remove text from footer div
-// // let elementToRemove = document.querySelector('footer > .footer');
-// // elementToRemove.parentElement.removeChild(elementToRemove);
-// // footerDiv.innerText = '';
-
-// let elementToEdit = document.querySelector('main :nth-child(3)');
-// elementToEdit.classList.remove('main');
-// elementToEdit.classList.add('footer');
